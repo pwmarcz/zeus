d4bc99e1fcbcd6275246968fdf783eac534bc9ae
Kostas Papadimitriou <kpap@grnet.gr>
2017-09-05 12:53:56 +0300

    Increase length of sms status field

---
+++ helios/migrations/0002_sms_delivery_status_20170807_1845.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('helios', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='voter',
+            name='last_sms_status',
+            field=models.CharField(max_length=255, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
