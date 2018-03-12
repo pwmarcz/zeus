c2d6eabf780189fb3d58e547264a57e12f361e3d
Paweł Marczewski <pwmarcz@gmail.com>
2018-03-12 20:49:33 +0100

    Move zeus.core.test_description where pytest can see it

--- zeus/core.py
+++ zeus/core.py
@@ -4823,59 +4823,6 @@ def main():
 
     teller_stream.flush()
 
-def test_decryption():
-    from zeus_sk import mix_ciphers
-    g = _default_crypto['generator']
-    p = _default_crypto['modulus']
-    q = _default_crypto['order']
-
-    texts = [0, 1, 2, 3, 4]
-    keys = [13, 14, 15, 16]
-    publics = [pow(g, x, p) for x in keys]
-    pk = 1
-    for y in publics:
-        pk = (pk * y) % p
-    cts = []
-    rands = []
-    for t in texts:
-        ct = encrypt(t, p, g, q, pk)
-        cts.append((ct[0], ct[1]))
-        rands.append(ct[2])
-
-    all_factors = []
-    for x in keys:
-        factors = compute_decryption_factors(p, g, q, x, cts)
-        all_factors.append(factors)
-
-    master_factors = combine_decryption_factors(p, all_factors)
-    pts = []
-    for (alpha, beta), factor in izip(cts, master_factors):
-        pts.append(decrypt_with_decryptor(p, g, q, beta, factor))
-    if pts != texts:
-        raise AssertionError("Z")
-
-    cfm = {'modulus': p,
-           'generator': g,
-           'order': q,
-           'public': pk,
-           'original_ciphers': cts,
-           'mixed_ciphers': cts}
-
-    mix1 = mix_ciphers(cfm)
-    mix = mix_ciphers(mix1)
-    cts = mix['mixed_ciphers']
-    all_factors = []
-    for x in keys:
-        factors = compute_decryption_factors(p, g, q, x, cts)
-        all_factors.append(factors)
-
-    master_factors = combine_decryption_factors(p, all_factors)
-    pts = []
-    for (alpha, beta), factor in izip(cts, master_factors):
-        pts.append(decrypt_with_decryptor(p, g, q, beta, factor))
-    if sorted(pts) != sorted(texts):
-        raise AssertionError("ZZ")
-
 retval = []
 
 if __name__ == '__main__':
