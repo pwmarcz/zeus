2d6189a50662db739dcbaf688a8493ee115a42c3
Maciej Gryka <maciej@rainforestapp.com>
2018-03-20 13:03:40 +0100

    add tests for zeus.views.utils

--- zeus/views/utils.py
+++ zeus/views/utils.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 def set_menu(menu, ctx):
     ctx['menu_active'] = menu
 
+
 def common_json_handler(obj):
     if hasattr(obj, 'isoformat'):
         return obj.isoformat()
