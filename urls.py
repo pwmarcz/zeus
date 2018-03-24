5042925957119eaa2f8919e80a4adeb13c443ffb
Paweł Marczewski <pwmarcz@gmail.com>
2018-03-24 22:13:34 +0100

    Use imported functions instead of function names in URLs
    Removed in Django 1.10.

--- urls.py
+++ urls.py
@@ -1,67 +1,66 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.conf import settings
+import django.views.i18n
+import django.views.static
 
-from zeus.views import admin
+from zeus.views import auth, admin, poll, site, shared
 
 SERVER_PREFIX = getattr(settings, 'SERVER_PREFIX', '')
 if SERVER_PREFIX:
     SERVER_PREFIX = SERVER_PREFIX.rstrip('/') + '/'
 
-app_patterns = patterns('')
+app_patterns = []
 
+auth_urls = [
+    url(r'^auth/logout', auth.logout, name='logout'),
+    url(r'^auth/login', auth.password_login_view, name='login'),
+    url(r'^auth/change_password', auth.change_password, name='change_password'),
+    url(r'^voter-login$', auth.voter_login, name="voter_login"),
+    url(r'^auth/oauth2$', auth.oauth2_login, name="oauth2_login"),
+    url(r'^auth/jwt$', auth.jwt_login, name="jwt_login"),
+    url(r'^auth/shibboleth/(?P<endpoint>.*)$', auth.shibboleth_login, name="shibboleth_login"),
+]
 
-auth_urls = patterns('zeus.views.auth',
-    url(r'^auth/logout', 'logout', name='logout'),
-    url(r'^auth/login', 'password_login_view', name='login'),
-    url(r'^auth/change_password', 'change_password', name='change_password'),
-    url(r'^voter-login$', 'voter_login', name="voter_login"),
-    url(r'^auth/oauth2$', 'oauth2_login', name="oauth2_login"),
-    url(r'^auth/jwt$', 'jwt_login', name="jwt_login"),
-    url(r'^auth/shibboleth/(?P<endpoint>.*)$', 'shibboleth_login', name="shibboleth_login"),
-)
-
-admin_urls = patterns('zeus.views.admin',
+admin_urls = [
     url(r'^$', admin.HomeView.as_view(), name='admin_home'),
-    url(r'^reports$', 'elections_report', name='elections_report'),
-    url(r'^reports/csv$', 'elections_report_csv', name='elections_report_csv'),
-)
+    url(r'^reports$', admin.elections_report, name='elections_report'),
+    url(r'^reports/csv$', admin.elections_report_csv, name='elections_report_csv'),
+]
 
-app_patterns += patterns(
-    '',
-    url(r'^vote', 'zeus.views.auth.voter_login', name='voter_quick_login'),
-    url(r'^f/(?P<fingerprint>.*)', 'zeus.views.poll.download_signature_short', name='download_signature_short'),
-    (r'^', include('zeus.urls.site')),
-    (r'^elections/', include('zeus.urls.election')),
-    (r'^auth/', include(auth_urls)),
-    (r'^admin/', include(admin_urls)),
-    url(r'^get-randomness/', 'zeus.views.shared.get_randomness',
+app_patterns += [
+    url(r'^vote', auth.voter_login, name='voter_quick_login'),
+    url(r'^f/(?P<fingerprint>.*)', poll.download_signature_short, name='download_signature_short'),
+    url(r'^', include('zeus.urls.site')),
+    url(r'^elections/', include('zeus.urls.election')),
+    url(r'^auth/', include(auth_urls)),
+    url(r'^admin/', include(admin_urls)),
+    url(r'^get-randomness/', shared.get_randomness,
         name="get_randomness"),
-    url(r'^i18n/js', 'django.views.i18n.javascript_catalog',
+    url(r'^i18n/js', django.views.i18n.javascript_catalog,
         name='js_messages', kwargs={'packages': None}),
-    (r'^i18n/setlang', 'zeus.views.site.setlang'),
-    (r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^i18n/setlang', site.setlang),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^account_administration/', include('account_administration.urls')),
-)
+]
 
-urlpatterns = patterns(
-    '',
-    (r'^' + SERVER_PREFIX, include(app_patterns)),
-)
+urlpatterns = [
+    url(r'^' + SERVER_PREFIX, include(app_patterns)),
+]
 
 #SHOULD BE REPLACED BY APACHE STATIC PATH
 if getattr(settings, 'DEBUG', False) or getattr(settings, 'ZEUS_SERVE_STATIC', False):
-    static_urls = patterns('',
-        (r'booth/(?P<path>.*)$', 'django.views.static.serve', {
+    static_urls = [
+        url(r'booth/(?P<path>.*)$', django.views.static.serve, {
             'document_root' : settings.BOOTH_STATIC_PATH
         }),
-        (r'static/zeus/(?P<path>.*)$', 'django.views.static.serve', {
+        url(r'static/zeus/(?P<path>.*)$', django.views.static.serve, {
             'document_root' : settings.ROOT_PATH + '/zeus/static/zeus'
         }),
-        (r'static/(?P<path>.*)$', 'django.views.static.serve', {
+        url(r'static/(?P<path>.*)$', django.views.static.serve, {
             'document_root' : settings.ROOT_PATH + '/server_ui/media'
         }),
-    )
+    ]
 
     urlpatterns += static_urls
 
