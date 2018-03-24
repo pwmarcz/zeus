64fdf9237ac369890e99a30ab82b40caff33d2ca
Paweł Marczewski <pwmarcz@gmail.com>
2018-03-24 21:57:37 +0100

    Remove ElectionInfo (unused)

--- zeus/models/zeus_models.py
+++ zeus/models/zeus_models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from helios import models as helios_models
-
 
 class Institution(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -9,22 +7,6 @@ class Institution(models.Model):
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        app_label = 'zeus'
-
-
-class ElectionInfo(models.Model):
-    uuid = models.CharField(max_length=50, null=False)
-    _election = None
-
-    @property
-    def election(self):
-        if self._election:
-            return self._election
-        else:
-            self._election = helios_models.Election.objects.get(uuid=self.uuid)
-            return self._election
 
     class Meta:
         app_label = 'zeus'
