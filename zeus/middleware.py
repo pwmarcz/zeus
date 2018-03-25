a6738b96e629dd9ddebbbae363509c2aefb27b6d
Paweł Marczewski <pwmarcz@gmail.com>
2018-03-25 21:25:50 +0200

    Convert middleware to new Django style

--- zeus/middleware.py
+++ zeus/middleware.py
@@ -5,9 +5,9 @@ from zeus import auth
 logger = logging.getLogger()
 
 
-class AuthenticationMiddleware(object):
+def AuthenticationMiddleware(get_response):
 
-    def process_request(self, request):
+    def middleware(request):
         setattr(request, 'zeususer', auth.ZeusUser(None))
 
         user = auth.ZeusUser.from_request(request)
@@ -19,7 +19,18 @@ class AuthenticationMiddleware(object):
         if user.is_trustee:
             setattr(request, 'trustee', user._user)
 
+        return get_response(request)
 
+    return middleware
+
+
+# TODO is this necessary?
 class ExceptionsMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
     def process_exception(self, request, exception):
         logger.exception(exception)
