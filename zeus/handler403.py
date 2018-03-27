87c895981ddb302fcc681a09cf755ca368a7a880
Paweł Marczewski <pwmarcz@gmail.com>
2018-03-27 23:09:58 +0200

    Remove zeus.handler403
    Replaced by zeus.views.site.handler403.

--- zeus/handler403.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-import sys
-
-def handler(request, *args, **kwargs):
-    exc_type, exc, exc_tb = sys.exc_info()
-    data = "403 Forbidden\n"
-    data += str(exc.args[0]) if exc is not None and exc.args else ""
-    return HttpResponse(data, content_type="text/plain", status=403)
