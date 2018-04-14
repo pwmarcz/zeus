4d9e8a1e69000dc8e04563b374056cb7484313d8
Paweł Marczewski <pwmarcz@gmail.com>
2018-04-14 17:47:40 +0200

    update-translations: use all langs

--- update-translations.sh
+++ update-translations.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+LANGS="-l en -l el -l pl"
+
 for d in zeus helios heliosauth server_ui account_administration; do
   echo $d
   (
     cd $d;
-    django-admin makemessages --no-location --all || true;
+    django-admin makemessages --no-location $LANGS || true;
   )
 done;
 
-python manage.py makeboothmessages --no-location --all;
+python manage.py makeboothmessages --no-location $LANGS;
