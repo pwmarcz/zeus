b4cffb96e97176f4bb6b6b3da9e3a0a04171586e
Paweł Marczewski <pwmarcz@gmail.com>
2018-04-14 17:45:00 +0200

    We want to look at .py files as well.

--- update-translations.sh
+++ update-translations.sh
@@ -4,8 +4,8 @@ for d in zeus helios heliosauth server_ui account_administration; do
   echo $d
   (
     cd $d;
-    django-admin makemessages --no-location -l en -l el -l pl -e .html -e .txt || true;
+    django-admin makemessages --no-location --all || true;
   )
 done;
 
-python manage.py makeboothmessages --no-location -l en -l el -l pl -e .html -e .js;
+python manage.py makeboothmessages --no-location --all;
