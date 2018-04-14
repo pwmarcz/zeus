49fcef0d8b5d21dcd0fd79715d62ee63227f5bdb
Nikola Adamus <nikola.adamus@edgenpd.com>
2018-04-14 16:08:00 +0200

    Add Polish language in settings
    Add new flags (type: shiny) from https://github.com/gosquared/flags/tree/master/flags/flags/shiny

--- settings/base.py
+++ settings/base.py
@@ -45,7 +45,7 @@ TIME_ZONE = 'Europe/Athens'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
-LANGUAGES = (('en', 'English'),)
+LANGUAGES = [('en', 'English'), ('el', 'Greek'), ('pl', 'Polish')]
 
 SITE_ID = 1
 
